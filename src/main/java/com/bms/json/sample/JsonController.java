0fe570451a656c883c4921021b57d6cc622c2cc7
sharaf31 <sharaf.navas@gmail.com>
2013-07-06 18:41:27 +0800

    added a sample git

---
+++ src/main/java/com/bms/json/sample/JsonController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.bms.json.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author sharaf
+ *
+ */
+@Controller
+@RequestMapping("/testrqst")
+public class JsonController {
+
+	
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+		model.addAttribute("movie", name);
+		return "list"; 
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getDefaultMovie(ModelMap model) { 
+		model.addAttribute("movie", "this is default movie");
+		return "list"; 
+	}
+	
+}
